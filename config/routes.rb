70b1d0022c6f7fe72263de72a9bfa8000ebb4660
AlexisDew <44058964+AlexisDew@users.noreply.github.com>
2019-02-18 17:45:48 +0100

    Update config/routes.rb
    Co-Authored-By: Aythabet <43108403+Aythabet@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :escorts, except: [:new, :create]
+  resources :escorts, except: [:new, :create, :update, :edit]
 end
