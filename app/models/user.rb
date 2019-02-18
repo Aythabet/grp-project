1ad5a57e3d6fec03a82f3c5f37a41ab79dde2e55
Nil <nilbesombes@gmail.com>
2019-02-18 17:37:15 +0100

    add pseudo age and validations to user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :reservations
   has_many :reviews, through: :reservation
+  validates :email, uniqueness: true
+  validates :pseudo, uniqueness: true
+  validates :age, inclusion: { in: 18..120 }
 end
