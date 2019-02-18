f4c9cef55ade6a151f8977b6405756c8861673a3
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-18 18:13:17 +0100

    redirect to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  # root to: 'pages#index'
+  root to: 'escorts#index'
   resources :escorts, except: [:new, :create, :update, :edit]
 end
