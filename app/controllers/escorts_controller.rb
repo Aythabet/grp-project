5354bdeb1540f3feddc74f2656456ddfa21c2523
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-19 15:58:33 +0100

    case insensitive

--- app/controllers/escorts_controller.rb
+++ app/controllers/escorts_controller.rb
@@ -1,7 +1,8 @@
 class EscortsController < ApplicationController
   def index
     if params[:query].present?
-      @escorts = Escort.where(pseudo: params[:query])
+     @escorts = Escort.where("pseudo ILIKE ?", "%#{params[:query]}%")
+      # @escorts = Escort.where(pseudo: params[:query])
     else
       @escorts = Escort.all
     end
