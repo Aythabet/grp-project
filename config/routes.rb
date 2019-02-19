4e7b129540819e87c483396f6ad141e69b024aee
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-02-19 11:46:27 +0100

    Photo Upload for Escort Profile
    added url to edit the escort show page where you can add pictures ! can be accessed escort/id/edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'escorts#index'
-  resources :escorts, except: [:new, :create, :update, :edit]
+  resources :escorts, except: [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_064457) do
+ActiveRecord::Schema.define(version: 2019_02_19_102957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_064457) do
     t.datetime "updated_at", null: false
     t.integer "age"
     t.string "contact"
+    t.string "photo"
   end
 
   create_table "reservations", force: :cascade do |t|
