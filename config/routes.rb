1cde94fbc1c359430a13b9fbe866d81660c32e1a
Nil <nilbesombes@gmail.com>
2019-02-19 14:46:16 +0100

    fix btn logout + btn 'profile' links to profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    get '/profile', to: 'devise/registrations#edit'
+  end
+
   devise_for :users
   root to: 'escorts#index'
   resources :escorts, except: [:new, :create, :update, :edit]
 end
+
+
+
