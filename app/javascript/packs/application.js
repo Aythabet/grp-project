cff2cd4c13ca04a64124699b15787dca411afe66
Nil <nilbesombes@gmail.com>
2019-02-21 15:20:04 +0100

    calendar added (not finished yet)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
 import "bootstrap";
+
+
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.css'
+
+console.log(flatpickr);
+
+
+const reservationDateElem = document.querySelector('#reservation_date')
+
+console.log(reservationDateElem)
+
+flatpickr(reservationDateElem);
