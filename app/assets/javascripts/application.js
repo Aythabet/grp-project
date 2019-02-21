3d3a905ca0901d929350484f99a92b92b29b6929
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-21 13:07:19 +0100

    remove debug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,6 @@
 const picture = document.querySelector('#user_picture');
 const form = document.querySelector('.edit_pic_form');
 
-console.log(form);
-
 picture.addEventListener('input', (e) => {
   form.submit()
 })
