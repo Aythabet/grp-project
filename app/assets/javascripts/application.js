71a61f926043b4eda5ffbffd4e830883a9c6a373
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-21 13:03:04 +0100

    add javascript to update pic when updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,12 @@
 //= require rails-ujs
 //= require_tree .
+
+
+const picture = document.querySelector('#user_picture');
+const form = document.querySelector('.edit_pic_form');
+
+console.log(form);
+
+picture.addEventListener('input', (e) => {
+  form.submit()
+})
