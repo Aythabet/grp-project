17f6e3c0e69b2f2d13704006d83caf1b9acc417f
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-22 14:26:02 +0100

    call init_mapbox from app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,9 @@ const reservationDateElem = document.querySelector('#reservation_date')
 console.log(reservationDateElem)
 
 flatpickr(reservationDateElem);
+
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
