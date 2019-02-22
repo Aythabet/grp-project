c2c8de7da854ea2c80f1b5bf67ff478d791e0750
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-02-22 07:21:16 +0100

    Added JS for bookings information hidden div

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,3 +10,28 @@ if (picture) {
     form.submit()
   })
 }
+
+
+//= padding add for the bookings badges infos
+
+function expandBooking() {
+  const hidden = document.querySelector('.booking-cards')
+  if (hidden) {
+  }
+  else {
+    hidden.insertAdjacentHTML("beforeend", `
+    <div class="booking-infos">
+      <p> <strong>Booking Infos:</strong></p >
+      <p>Escort Reserved: <%=reservation.escort.pseudo %></p>
+      <p>Last message: <%=reservation.message %></p>
+      <p>Booking Date: <%=reservation.date %></p>
+    </div>`);;;
+  }
+}
+}
+
+hidden.addEventListener(mouseover, (event) => {
+  expandBooking(),
+});
+
+
