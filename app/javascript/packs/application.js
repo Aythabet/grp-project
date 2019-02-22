51a2be2614d9370c7777cf4e5ad45dd849a1626b
Ayoub Ben Thabet <43108403+Aythabet@users.noreply.github.com>
2019-02-22 14:43:08 +0100

    Delete application.js

--- app/javascript/packs/application.js
+++
@@ -1,9 +0,0 @@
-import "bootstrap";
-
-
-import flatpickr from "flatpickr";
-import 'flatpickr/dist/flatpickr.css'
-
-const reservationDateElem = document.querySelector('#reservation_date')
-
-flatpickr(reservationDateElem);
