7959f665da9be045ed8b24517c44b49cfcdf07cf
Nil <nilbesombes@gmail.com>
2019-02-22 14:33:26 +0100

    fix logout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,6 @@ import "bootstrap";
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.css'
 
-console.log(flatpickr);
-
-
 const reservationDateElem = document.querySelector('#reservation_date')
-
-console.log(reservationDateElem)
 
 flatpickr(reservationDateElem);
