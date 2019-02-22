deb994b21d336cbc594f6333791e756b30deacd5
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-02-22 11:15:42 +0100

    Booking info

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-
-
-const picture = document.querySelector('#user_picture');
-const form = document.querySelector('.edit_pic_form');
-
-if (picture) {
-  picture.addEventListener('input', (e) => {
-    form.submit()
-  })
-}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,26 @@ const reservationDateElem = document.querySelector('#reservation_date')
 console.log(reservationDateElem)
 
 flatpickr(reservationDateElem);
+
+const picture = document.querySelector('#user_picture');
+const form = document.querySelector('.edit_pic_form');
+
+if (picture) {
+  picture.addEventListener('input', (e) => {
+    form.submit();
+  });
+}
+
+//= padding add for the bookings badges infos
+
+const hidden = document.querySelector('.booking-infos');
+
+if (hidden) {
+  hidden.addEventListener('click', () => {
+    if (hidden.style.display = "none") {
+      hidden.style.display = "block";
+    } else {
+      hidden.style.display = "none";
+    }
+  });
+}
